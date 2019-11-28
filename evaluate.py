e601344fc331cbcc93434d6f571294a8c1a407cd
Yashas Annadani <yashas.annadani@gmail.com>
2019-11-28 14:19:35 +0100

    refactor the evaluation code

--- evaluate.py
+++ evaluate.py
@@ -40,27 +40,32 @@ def _save_to_text(save_path, results):
 			t.write('\n\n')
 	return 0
 
+def _assert_args_and_load(model, args, dataset, representation_function):
+	if not isinstance(model, nn.Module)  and not isinstance(model,str):
+		raise Exception('Should provide the model or the path to the model, but got {}'.format(model.__class__.__name__))
+	if isinstance(model, str) and representation_function is None:
+		args, model = _load_model_eval(model, args)
+		model.eval()
+		representation_function = representation_func(model)
+	if dataset is None:
+		dataset = dsprites.DSprites()
+	return representation_function, dataset
+		
+
 
 class representation_func(object):
-	def __init__(self, model, latent_dim):
-		self.latent_dim = latent_dim
-		self.model = model
+	def __init__(self, model):
+		self.model = model.enc
 	def __call__(self, x):
 		x = np.transpose(x, (0,3,2,1))
 		"""Computes representation vector for input images."""
-		output = self.model.encode(torch.Tensor(x))
-		if isinstance(self.model.enc, models.Normal_Conv_Encoder):
+		output = self.model(torch.Tensor(x))
+		if isinstance(self.model, models.Normal_Conv_Encoder):
 			output = output.loc
 		return output.detach().numpy()
 
 def eval_all(model = None, args = None, dataset = None, representation_function = None, seed = 10, save_path = None):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	results = {}
 	for id_, metric in enumerate(ALL_METRICS):
 		print('Evaluating {}'.format(metric))
@@ -73,102 +78,55 @@ def eval_all(model = None, args = None, dataset = None, representation_function 
 		
 
 def eval_beta_vae(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return metric_beta_vae.compute_beta_vae_sklearn(dataset, representation_function, np.random, 64, 10000, 5000)
 
 def eval_factor_vae(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return metric_factor_vae.compute_factor_vae(dataset, representation_function, np.random, 64, 10000, 5000, 10000)
 
 def eval_mig(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return mig.compute_mig(dataset, representation_function, np.random, 10000)
 
 
 def eval_dci(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return dci.compute_dci(dataset, representation_function, np.random, 10000, 5000)
 
 def eval_irs(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return irs.compute_irs(dataset, representation_function, np.random)
 
 def eval_sap(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return sap.compute_sap(dataset, representation_function, np.random, 10000, 5000, False)
 
 def eval_modularity_explicitness(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return modularity_explicitness.compute_modularity_explicitness(dataset, _representation_function, np.random, 10000, 5000, 64)
 
 def eval_unsupervised(model = None, args = None, dataset = None, representation_function = None, seed = 10):
-	if isinstance(model, str) and representation_function is None:
-		args, model = _load_model_eval(model, args)
-	model.eval()
-	if dataset is None:
-		dataset = dsprites.DSprites()
-	if representation_function is None:
-		representation_function = representation_func(model, args['model']['latent_dim'])
+	representation_function, dataset = _assert_args_and_load(model, args, dataset, representation_function)
 	np.random.seed(seed) 
 	return unsupervised_metrics.unsupervised_metrics(dataset, representation_function, np.random, 10000)
 
 
 
-ALL_METRICS = ['Beta VAE Score', 'Factor VAE Score', 'MIG', 'DCI', 'IRS', 'SAP', 'Modularity Explicitness', 'Unsupervised Metrics']#, 'FID'] #TODO: ADD Fid score
-ALL_METRIC_FUNC = [eval_beta_vae, eval_factor_vae, eval_mig, eval_dci, eval_irs, eval_sap, eval_modularity_explicitness, eval_unsupervised]
-
 if __name__ == '__main__':
 	args = parse_args()
 	if args.save_path is None:
 		args.save_path = args.model
+
+	ALL_METRICS = ['Beta VAE Score', 'Factor VAE Score', 'MIG', 'DCI', 'IRS', 'SAP', 'Modularity Explicitness', 'Unsupervised Metrics']#, 'FID'] #TODO: ADD Fid score
+	ALL_METRIC_FUNC = [eval_beta_vae, eval_factor_vae, eval_mig, eval_dci, eval_irs, eval_sap, eval_modularity_explicitness, eval_unsupervised]
+	
 	if args.all:
 		eval_all(model = args.model, seed = args.seed, save_path = args.save_path)
